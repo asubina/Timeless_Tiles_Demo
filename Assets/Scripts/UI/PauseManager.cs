722583679090f988978ba5893ad0d983d2953c77
asubina <a.n.n.a.shubina13@gmail.com>
2024-05-22 16:17:39 +0100

    Sound On/Off finished

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -3,18 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor.Experimental.GraphView;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject pausePanel;
     private Board board;
     public bool paused = false;
+    public Image soundButton;
+    public Sprite musicOnSprite;
+    public Sprite musicOffSprite;
+    private SoundManager sound; 
 
     // Start is called before the first frame update
     void Start()
     {
-        pausePanel.SetActive(false);
+        sound = FindObjectOfType<SoundManager>();
         board = GameObject.FindWithTag("Board").GetComponent<Board>();
+        pausePanel.SetActive(false);
+        if (PlayerPrefs.HasKey("Sound")){
+            if(PlayerPrefs.GetInt("Sound") == 0)
+            {
+                soundButton.sprite = musicOffSprite;
+            }
+            else
+            {
+                soundButton.sprite = musicOnSprite;
+            }
+        }
+        else
+        {
+            soundButton.sprite = musicOnSprite;
+        }
+    }
+
+    public void SoundButton()
+    {
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            if (PlayerPrefs.GetInt("Sound") == 0)
+            {
+                PlayerPrefs.SetInt("Sound", 1);
+                soundButton.sprite = musicOnSprite;
+                sound.AdjustVolume();
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Sound", 0);
+                soundButton.sprite = musicOffSprite;
+                sound.AdjustVolume();
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Sound", 0);
+            soundButton.sprite = musicOffSprite;
+            sound.AdjustVolume();
+        }
     }
 
     // Update is called once per frame
