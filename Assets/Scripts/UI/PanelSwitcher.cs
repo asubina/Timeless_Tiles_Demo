4fccb47ebdcc8243275d55eb5b88df270c53101e
asubina <a.n.n.a.shubina13@gmail.com>
2024-05-23 21:39:20 +0100

    Start-level-design panels logic implemented

--- Assets/Scripts/UI/PanelSwitcher.cs
+++ Assets/Scripts/UI/PanelSwitcher.cs
@@ -5,43 +5,45 @@ using UnityEngine;
 public class PanelSwitcher : MonoBehaviour
 {
     public GameObject startPanel;
+    public GameObject designPanel;
     public GameObject levelPanel;
 
     void Start()
     {
-        InitializePanels();
-    }
-
-    void InitializePanels()
-    {
-        if (PlayerPrefs.GetInt("GoToLevelSelect", 0) == 1)
-        {
-            ShowLevelPanel();
-            PlayerPrefs.SetInt("GoToLevelSelect", 0); // Reset the flag
-        }
-        else
-        {
-            ShowStartPanel();
-        }
+        // Optionally, you can set the initial panel to be active
+        ShowStartPanel(); // Or whichever panel you want to be the default
     }
 
     public void ShowStartPanel()
     {
-        if (startPanel != null && levelPanel != null)
+        if (startPanel != null && designPanel != null && levelPanel != null)
         {
             startPanel.SetActive(true);
+            designPanel.SetActive(false);
             levelPanel.SetActive(false);
-            // Debug.Log("ShowStartPanel: Start panel activated, LevelSelect panel deactivated.");
+            // Debug.Log("ShowStartPanel: Start panel activated, Design and Level panels deactivated.");
+        }
+    }
+
+    public void ShowDesignPanel()
+    {
+        if (designPanel != null && startPanel != null && levelPanel != null)
+        {
+            designPanel.SetActive(true);
+            startPanel.SetActive(false);
+            levelPanel.SetActive(false);
+            // Debug.Log("ShowDesignPanel: Design panel activated, Start and Level panels deactivated.");
         }
     }
 
     public void ShowLevelPanel()
     {
-        if (startPanel != null && levelPanel != null)
+        if (levelPanel != null && startPanel != null && designPanel != null)
         {
-            startPanel.SetActive(false);
             levelPanel.SetActive(true);
-            // Debug.Log("ShowLevelPanel: LevelSelect panel activated, Start panel deactivated.");
+            startPanel.SetActive(false);
+            designPanel.SetActive(false);
+            // Debug.Log("ShowLevelPanel: Level panel activated, Start and Design panels deactivated.");
         }
     }
 }
