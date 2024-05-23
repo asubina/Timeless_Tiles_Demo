2b765e04e5e9608946c9ce27ad479d0d28688d48
asubina <a.n.n.a.shubina13@gmail.com>
2024-05-23 21:48:47 +0100

    Rules panel wired up

--- Assets/Scripts/UI/PanelSwitcher.cs
+++ Assets/Scripts/UI/PanelSwitcher.cs
@@ -6,6 +6,7 @@ public class PanelSwitcher : MonoBehaviour
 {
     public GameObject startPanel;
     public GameObject designPanel;
+    public GameObject rulesPanel;
     public GameObject levelPanel;
 
     void Start()
@@ -16,34 +17,49 @@ public class PanelSwitcher : MonoBehaviour
 
     public void ShowStartPanel()
     {
-        if (startPanel != null && designPanel != null && levelPanel != null)
+        if (startPanel != null && designPanel != null && levelPanel != null && rulesPanel != null)
         {
             startPanel.SetActive(true);
             designPanel.SetActive(false);
             levelPanel.SetActive(false);
+            rulesPanel.SetActive(false);
             // Debug.Log("ShowStartPanel: Start panel activated, Design and Level panels deactivated.");
         }
     }
 
     public void ShowDesignPanel()
     {
-        if (designPanel != null && startPanel != null && levelPanel != null)
+        if (designPanel != null && startPanel != null && levelPanel != null && rulesPanel != null)
         {
             designPanel.SetActive(true);
             startPanel.SetActive(false);
             levelPanel.SetActive(false);
+            rulesPanel.SetActive(false);
             // Debug.Log("ShowDesignPanel: Design panel activated, Start and Level panels deactivated.");
         }
     }
 
     public void ShowLevelPanel()
     {
-        if (levelPanel != null && startPanel != null && designPanel != null)
+        if (levelPanel != null && startPanel != null && designPanel != null && rulesPanel != null)
         {
             levelPanel.SetActive(true);
             startPanel.SetActive(false);
             designPanel.SetActive(false);
+            rulesPanel.SetActive (false);
             // Debug.Log("ShowLevelPanel: Level panel activated, Start and Design panels deactivated.");
+        }
+    }
+
+    public void ShowRulesPanel()
+    {
+        if(startPanel != null && designPanel != null && levelPanel != null && rulesPanel != null)
+        {
+            rulesPanel.SetActive(true);
+            startPanel.SetActive(false);
+            designPanel.SetActive(false);
+            levelPanel.SetActive(false);
+
         }
     }
 }
